525b73eeb0665feb965562c5a491dd523e5246a2
gilberto-olimpio <69879651+gilberto-olimpio@users.noreply.github.com>
2026-09-11 14:58:47 -0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GEOPOWER_CONFIG = {
-  SUPABASE_URL: 'https://SEU-PROJETO.supabase.co',
+  SUPABASE_URL: 'https://muwsdmshxucffjiqhwfu.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_-m0Q5rCKZtzs44vjzJ1aSQ_pOhVrbJW'
 };
